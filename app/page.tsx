db65a360329a592f181fd98621e4945c294a15ee
Nikolay <nikolay.tetradov@gmail.com>
2025-09-04 14:39:34 +0300

    clerk SignInButton added to main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import { SignInButton } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Logo from '../assets/Colour-Logo_noBackground.svg';
@@ -20,7 +20,10 @@ export default function Home() {
             where financial empowerment meets simplicity
           </p>
           <Button asChild className='mt-4'>
-            <Link href='/investments'>Get Started</Link>
+            <SignInButton>
+              Get Started
+              {/* <Link href='/investments'>Get Started</Link> */}
+            </SignInButton>
           </Button>
         </div>
         <Image
