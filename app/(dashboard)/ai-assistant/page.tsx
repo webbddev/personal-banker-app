60491ee37c3314ef8c6bc2cf86b84e3f71b5bad6
Nikolay <nikolay.tetradov@gmail.com>
2025-10-30 19:52:45 +0200

    ai chatbox modifications

--- app/(dashboard)/ai-assistant/page.tsx
+++ app/(dashboard)/ai-assistant/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
 import AIChat from '@/components/AiChat';
 import { SiteHeader } from '@/components/site-header';
 import { SidebarInset } from '@/components/ui/sidebar';
+import { useState } from 'react';
+import { useChat } from '@ai-sdk/react';
 
 const AiPage = () => {
+  const [isCopied, setIsCopied] = useState<Record<string, boolean>>({});
+
+  const { messages, status, error, regenerate } = useChat();
+
   return (
     <SidebarInset className='w-full'>
       <SiteHeader title='AI Assistant' />
@@ -10,7 +18,14 @@ const AiPage = () => {
         <div className='flex flex-1 flex-col gap-2 w-full'>
           <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6 w-full'>
             <div className='w-full'>
-              <AIChat />
+              <AIChat
+                messages={messages}
+                status={status}
+                error={error}
+                regenerate={regenerate}
+                isCopied={isCopied}
+                setIsCopied={setIsCopied}
+              />
             </div>
           </div>
         </div>
